4a67e670c456882700219b9eedb8b615619fedfd
kacper <kacper.tarka00@gmail.com>
2023-06-11 23:51:34 +0200

    New User functionality

--- base/userProfile/api/serializers.py
+++ base/userProfile/api/serializers.py
@@ -12,3 +12,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email')
+        write_only_fields = ('password',)
+        read_only_fields = ('id', )
+        
+    def create(self, validated_data):
+        user = User.objects.create(
+            email = validated_data.get('email'),
+            first_name = validated_data.get('first_name'),
+            last_name = validated_data.get('last_name'),
+            
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        
+        return user
--- base/userProfile/api/views.py
+++ base/userProfile/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .serializers import UserSerializer
 from userProfile.models import User
@@ -10,3 +12,14 @@ class UsersViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    def create(self, request):
+        """
+        Create new user
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+        return Response(serializer.data)
+    
